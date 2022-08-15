022eb8ef9efe5f8931f2315d9f4f5c9d311d9fc7
ArthurBDemiciano <arthurbdemiciano@gmail.com>
2022-08-15 10:53:10 -0300

    Configuração de servidor antes do python

---
+++ exemplos/day1/http_client.py
@@ -0,0 +1,16 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("localhost", 9000))
+
+#request
+cmd = "GET http://localhost.com/index.html HTTP/1.0\r\n\r\n".encode()
+client.send(cmd)
+
+while True:
+    data = client.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end="")
+
+client.close()
---
+++ exemplos/day1/script.js
@@ -0,0 +1,5 @@
+function emite_alerta(){
+    alert("Python Rocks!!")
+}
+logo = document.getElementsByTagName("img")[0]
+logo.onclick = emite_alerta;
